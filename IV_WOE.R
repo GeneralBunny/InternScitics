f443859ffc217b5ac74a66f9e4fea307042ab14a
GeneralBunny <wangxi1210@gmail.com>
2017-04-04 20:55:56 -0400

    calculate IV and replace with WOE

--- IV_WOE.R
+++ IV_WOE.R
@@ -11,9 +11,7 @@ library(glmnet)
 
 Agg_all <- read.csv("Agg_all_3.csv", header = TRUE);
 
-#B_OdTy_Other_12mos and B_OdTy_Other_6mos, all 0's.
-# Not included in Agg_all.csv
-Table <- file("table.csv", "w") 
+Table <- file("table.csv", "w")
 write.table_with_header <- function(x, file, h){
         writeLines(h, file)
         write.csv(x,file,row.names = FALSE);
@@ -39,7 +37,7 @@ IV <- create_infotables(data=Agg_all[,3:ncol(Agg_all)], y = "Renew", parallel = 
 # IV$Summary shows the IV of all variables.
 IVsMR<-IV$Summary;
 print(head(IVsMR));
-# write.csv(IVsMR,"IV_Summary.csv", row.names = FALSE);
+write.csv(IVsMR,"IV_Summary.csv", row.names = FALSE);
 
 # IV$Tables is a list of the IV and WOE for all the variables.
 for (i in 1:length(IV$Tables)){
@@ -58,7 +56,7 @@ for (i in 1:length(IV$Tables)){
         }}
 
 # for plot_infotables, show_values = TRUE does not work in the "Information" package.
-plot_infotables(IV, IV$Summary$Variable[1:5],same_scales = TRUE,show_values=TRUE);
+# plot_infotables(IV, IV$Summary$Variable[1:5],same_scales = TRUE,show_values=TRUE);
 
 
 # print(IV$Tables$B_order_12mos);
@@ -69,3 +67,31 @@ plot_infotables(IV, IV$Summary$Variable[1:5],same_scales = TRUE,show_values=TRUE
 # plot WOE of several variables.
 
 # MultiPlot(IV, names[3:6],same_scales=TRUE);
+
+# replace the value by WOE value.
+Agg_all_WOE <- Agg_all[FALSE,4:ncol(Agg_all)];
+
+for (i in 1:length(IV$Tables)){
+        w<- IV$Tables[[i]];
+        range <- data.frame(WOE = numeric(nrow(w)));
+        range$WOE[1] <- w$WOE[1];
+        range$interval[1] <- NA;
+        for (j in 2:nrow(w)){
+                num <- substr(w[j,1], start = 2, stop = nchar(w[j,1])-1);
+                num <- strsplit(num, ",");
+                # vector should be stored as list in dataframe
+                range$interval[j]<- list(c(as.numeric(num[[1]][1]), as.numeric(num[[1]][2])));
+                range$WOE[j] <- w$WOE[j];
+        }
+        
+        for (k in 1:nrow(Agg_all)){     
+                for (l in 2:nrow(range)){
+                        if (is.na(findInterval(Agg_all[k,i+3], unlist(range$interval[l]), rightmost.closed = TRUE)))
+                        {Agg_all_WOE[k,i] <- range$WOE[1]}
+                                else if (findInterval(Agg_all[k,i+3], unlist(range$interval[l]), rightmost.closed = TRUE)==1)
+                                        {Agg_all_WOE[k,i] <- range$WOE[l]}
+                        }
+                }
+}
+
+write.csv(Agg_all_WOE, "Agg_all_WOE.csv",row.names=FALSE);
